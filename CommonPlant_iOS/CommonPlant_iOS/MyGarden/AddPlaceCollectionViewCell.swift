c7fce08324fb4064b9d1ece1b598b938b75eda21
yaewonLee <yw990324@gmail.com>
2023-01-17 20:32:10 +0900

    feat: add place CollectionView UI and files

---
+++ CommonPlant_iOS/CommonPlant_iOS/MyGarden/AddPlaceCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  AddPlaceCollectionViewCell.swift
+//  CommonPlant_iOS
+//
+//  Created by 이예원 on 2023/01/17.
+//
+
+import UIKit
+
+class AddPlaceCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var addPlaceImg: UIImageView!
+}
--- CommonPlant_iOS/CommonPlant_iOS/MyGarden/MainViewController.swift
+++ CommonPlant_iOS/CommonPlant_iOS/MyGarden/MainViewController.swift
@@ -7,23 +7,27 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
-
+class MainViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "AddPlaceCollectionViewCell", for: indexPath) as? AddPlaceCollectionViewCell else { return UICollectionViewCell() }
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 250, height: 156)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
