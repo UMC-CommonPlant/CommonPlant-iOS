aab33905b08afa7d5a08af4d4fff4f4a66aa899d
yaewonLee <yw990324@gmail.com>
2023-01-21 17:43:07 +0900

    feat: customize tab bar

--- CommonPlant_iOS/CommonPlant_iOS/TabBarController.swift
+++ CommonPlant_iOS/CommonPlant_iOS/TabBarController.swift
@@ -8,24 +8,31 @@
 import UIKit
 
 class TabBarController: UITabBarController {
-
+    
+    var borderView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
     
-    override func viewWillAppear() {
-        super.viewWillAppear()
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.setUpTabBar()
+    }
+
+    func setUpTabBar() {
+        tabBar.frame.size.height = 98
+        tabBar.frame.origin.y = view.frame.size.height - 98
+        tabBar.layer.borderWidth = 1
+        tabBar.layer.borderColor = UIColor(red: 0.879, green: 0.879, blue: 0.879, alpha: 1).cgColor
         
-        UITabBar.appearance()
+        self.view.layoutIfNeeded()
     }
     
-}
-
-extension UITabBar {
-    override open func sizeThatFits(_ size: CGSize) -> CGSize {
-        var sizeThatFits =  super.sizeThatFits(size)
-        sizeThatFits.height = 64
-        return sizeThatFits
-    }
+//
+//    func sizeThatFits(_ size: CGSize) -> CGSize {
+//        var sizeThatFits =  super.sizeThatFits(size)
+//        sizeThatFits.height = 64
+//        return sizeThatFits
+//    }
 }
