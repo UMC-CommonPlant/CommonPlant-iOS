ca6616aafec2af6968389f296766e681b8c9f087
음휘영 <76817418+hweyoung@users.noreply.github.com>
2023-01-22 11:25:05 +0900

    Revert "Okbear3"

--- CommonPlant_iOS/CommonPlant_iOS/PlantCollectionViewCell.swift
+++ CommonPlant_iOS/CommonPlant_iOS/PlantCollectionViewCell.swift
@@ -8,40 +8,7 @@
 import UIKit
 
 class PlantCollectionViewCell: UICollectionViewCell {
-
-    @IBOutlet weak var categoryImageView: UIImageView!
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var categoryView: UIView!
     
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    
-    func setupData(
-        _ categoryImage: UIImage?,
-        _ title: String,
-        _ color: UIColor?
-    ){
-        if let categoryImage = categoryImage{
-            categoryImageView.image = categoryImage
-        }else{
-            categoryImageView.image = UIImage(named: "InfoPlantImg")
-        }
-        
-        titleLabel.text = title
-        
-        if let color = color{
-            categoryView.backgroundColor = color
-            categoryView.layer.cornerRadius = 8
-            print("item")
-        }else{
-            categoryView.backgroundColor = UIColor(named: "infoCategoryColor1")
-            print("infoCategoryColor1")
-        }
-        
-    }
     
 }
