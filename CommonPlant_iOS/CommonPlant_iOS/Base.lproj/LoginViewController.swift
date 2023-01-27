a78c7dc54cb0bb932dfd6f5d5888125780a0361d
hweyoung <gnldud0516@gmail.com>
2023-01-27 20:57:41 +0900

    Feat : update login UI

--- CommonPlant_iOS/CommonPlant_iOS/Base.lproj/LoginViewController.swift
+++ CommonPlant_iOS/CommonPlant_iOS/Base.lproj/LoginViewController.swift
@@ -18,10 +18,30 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    
+    //카카오 계정으로 로그인 시 -> 로그인 API 호출
+    //join : 사용자 정보와 함께 "1-2-2 Login" 으로 넘어감 -> 사용자 정보와 함께 join -> userDefault에 jwt token 저장 후 "Main"으로 넘어감
+    //login : userDefault에 jwt token 저장 후 "Main"으로 넘어감
     @IBAction func kakaoLoginAction(_ sender: UIButton){
         print("kakao Login")
-        //카카오 계정으로 로그인할 때
-        UserApi.shared.loginWithKakaoAccount {(oauthToken, error) in
+        //앱에 카카오톡이 설치되어 있는지 확인
+        if(UserApi.isKakaoTalkLoginAvailable()){
+            //카카오톡으로 로그인(권장)
+            UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
+                    if let error = error {
+                        print(error)
+                    }
+                    else {
+                        print("loginWithKakaoTalk() success.")
+
+                        //do something
+                        _ = oauthToken
+                    }
+                }
+        }else{
+            //카카오 계정으로 로그인할 때
+            print("계정으로 로그인")
+            UserApi.shared.loginWithKakaoAccount {(oauthToken, error) in
                 if let error = error {
                     print(error)
                 }
@@ -34,7 +54,9 @@ class LoginViewController: UIViewController {
                     print(accessToken)
                 }
             }
-        print("test")
+        }
+        
+       
     }
     
     
--- CommonPlant_iOS/CommonPlant_iOS/NicknameViewController.swift
+++ CommonPlant_iOS/CommonPlant_iOS/NicknameViewController.swift
@@ -6,9 +6,24 @@
 //
 
 import UIKit
+import Photos
 
-class NicknameViewController: UIViewController {
+class NicknameViewController: UIViewController{
+    
+    @IBOutlet weak var profileImage: UIImageView!
 
+    
+    var cnt = 0
+
+    
+    @IBAction func alertBtn(_ sender: Any) {
+        actionSheetAlert()
+        
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -37,4 +52,80 @@ class NicknameViewController: UIViewController {
     }
     */
 
+}
+
+// UIImagePickerControllerDelegate = 카메라 롤이나 앨범에서 사진을 가져올 수 있도록 도와 주는 것
+extension NicknameViewController : UIImagePickerControllerDelegate , UINavigationControllerDelegate{
+    
+    func actionSheetAlert(){
+        
+        let alert = UIAlertController(title: "선택", message: "선택", preferredStyle: .actionSheet)
+        
+        let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        let camera = UIAlertAction(title: "카메라", style: .default) { [weak self] (_) in
+            self?.presentCamera()
+        }
+        let album = UIAlertAction(title: "앨범", style: .default) { [weak self] (_) in
+            self?.presentAlbum()
+        }
+        
+        alert.addAction(cancel)
+        alert.addAction(camera)
+        alert.addAction(album)
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    func presentCamera(){
+        
+        let vc = UIImagePickerController()
+        vc.sourceType = .camera
+        vc.delegate = self
+        vc.allowsEditing = true
+        vc.cameraFlashMode = .on
+        
+        present(vc, animated: true, completion: nil)
+    }
+    
+    func presentAlbum(){
+        
+        
+        let vc = UIImagePickerController()
+        vc.sourceType = .photoLibrary
+        vc.delegate = self
+        vc.allowsEditing = true
+        
+        present(vc, animated: true, completion: nil)
+    }
+    
+    
+    //카메라나 앨범등 PickerController가 사용되고 이미지 촬영을 했을 때 발동 된다.
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        print("picker -> \(String(describing: info[UIImagePickerController.InfoKey.imageURL]))")
+   
+        if cnt % 2 == 0 {
+
+            if let image = info[.editedImage] as? UIImage {
+                profileImage.image = image
+            }
+
+        }
+        else{
+
+            if let image = info[.originalImage] as? UIImage {
+                profileImage.image = image
+            }
+
+        }
+        
+        cnt += 1
+        
+        print(cnt)
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
 }
