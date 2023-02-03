3d46910bcddbf741b85356e740a2c8ff480c970c
yaewonLee <yw990324@gmail.com>
2023-02-04 01:19:37 +0900

    feat: add addPlace ViewController UI

--- CommonPlant_iOS/CommonPlant_iOS/MyGarden/AddPlaceVC.swift
+++ CommonPlant_iOS/CommonPlant_iOS/MyGarden/AddPlaceVC.swift
@@ -9,21 +9,30 @@ import UIKit
 
 class AddPlaceVC: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var addressBtn: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        textFieldUnderline()
+        buttonUnderLine()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func textFieldUnderline() {
+        let bottomLine = CALayer()
+        bottomLine.frame = CGRect(x: 0.0, y: textField.frame.height - 1, width: textField.frame.width, height: 1.0)
+        bottomLine.backgroundColor = UIColor(named: "Gray2")?.cgColor
+        textField.borderStyle = UITextField.BorderStyle.none
+        textField.layer.addSublayer(bottomLine)
     }
-    */
+    
+    func buttonUnderLine() {
+        let bottomLine = CALayer()
+        bottomLine.frame = CGRect(x: 0.0, y: addressBtn.frame.height - 1, width: addressBtn.frame.width, height: 1.0)
+        bottomLine.backgroundColor = UIColor(named: "Gray2")?.cgColor
+        addressBtn.layer.addSublayer(bottomLine)
+    }
+ 
 
 }
