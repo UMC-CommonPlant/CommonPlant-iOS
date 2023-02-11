bfd24cdafed545e7403125b6dba59806f4d06fd2
Yaewon Lee <yw990324@gmail.com>
2023-02-12 02:13:51 +0900

    chore: edit Models

---
+++ CommonPlant_iOS/CommonPlant_iOS/MyGarden/Model/MyPlaceModel.swift
@@ -0,0 +1,38 @@
+//
+//  MyGardenModel.swift
+//  CommonPlant_iOS
+//
+//  Created by 이예원 on 2023/01/20.
+//
+
+import Foundation
+
+struct MyPlaceModel: Codable {
+    let timeStamp: String
+    let status: Int
+    let message: String
+    let result: MyPlaceResult
+    let success: Bool
+}
+
+struct MyPlaceResult: Codable {
+    let name, address, highestTemp, minimumTemp: String
+    let humidity: String
+    let isOwner: Bool
+    let userInfoList: [UserInfoList]
+    let plantInfoList: [PlantInfoList]
+}
+
+struct PlantInfoList: Codable {
+    let name, nickname: String
+    let imgURL: String?
+    let recentMemo: String
+    let remainderDate: Int
+    let wateredDate: String
+}
+
+struct UserInfoList: Codable {
+    let nickName: String
+    let owner: Bool
+    let imgURL: String?
+}
