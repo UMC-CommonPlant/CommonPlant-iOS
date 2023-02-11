2a3f01175538b25a5f47d5c8677b733c4e13c1b5
Yaewon Lee <yw990324@gmail.com>
2023-02-11 22:47:30 +0900

    feat: add AddPlant2ndVC and updated UI

---
+++ CommonPlant_iOS/CommonPlant_iOS/MyGarden/MyPlant/AddPlant2ndVC.swift
@@ -0,0 +1,17 @@
+//
+//  AddPlant2ndViewController.swift
+//  CommonPlant_iOS
+//
+//  Created by 이예원 on 2023/02/11.
+//
+
+import UIKit
+
+class AddPlant2ndVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
